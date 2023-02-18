036846508413f86c97eeb460810251981e844552
mizso42 <85360731+mizso42@users.noreply.github.com>
2023-02-18 09:40:38 +0100

    Bugfix

--- test_deloitte/test_sql_deloitte.sql
+++ test_deloitte/test_sql_deloitte.sql
@@ -1,19 +1,19 @@
 -- Task 1
-SELECT Login, Name, MIN(LastLogin, User_B.LoginDate) as [Last known login]
+SELECT Login, Name, MAX(LastLogin, User_B.LoginDate) as [Last known login]
 	from User_A JOIN User_B
     	on User_A.Login = User_B.Login_ID
     GROUP by login
 ;
 
 -- Task 2
-SELECT Login, Name, lastlogin AS [Last known login]
+SELECT Login, Name, MAX(lastlogin) AS [Last known login]
 	FROM User_A
     	WHERE login not in (
           SELECT login_id from User_B
         )
     GROUP BY login
 UNION
-SELECT login_id, full_name, MIN(logindate)
+SELECT login_id, full_name, MAX(logindate)
 	FROM User_B
     	WHERE login_id not in (
           SELECT login from User_A
